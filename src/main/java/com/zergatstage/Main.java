cd323ba1375b29b005c6b68428981461889a536f
zzergAtStage <sbrusentsov@biarum.com>
2025-01-12 23:44:23 +0100

    dev: First version with simple tracking 0.0.1-SNAPSHOT

---
+++ src/main/java/com/zergatstage/Main.java
@@ -0,0 +1,11 @@
+package com.zergatstage;
+
+/**
+ * @author S.Brusentsov
+ * date on 12/01/2025
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
